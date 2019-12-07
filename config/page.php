20704c23cbf431ee64b3f56e9a7b54dd26611b4b
Kolsyra <kolsyra@Kolsyra.local>
2019-12-07 16:00:16 +0100

    Kmom03 commit

--- config/page.php
+++ config/page.php
@@ -20,7 +20,7 @@ return [
             "stylesheets" => [
                 // Change here to set another default stylesheet
                 //"css/dbwebb-se.min.css",
-                "css/kmom02.min.css",
+                "css/kmom03.min.css",
             ],
             "javascripts" => [
                 "js/responsive-menu.js",
